4caa6451b88cdb5eec13ee81c6794ee01358c7e6
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2021-12-19 18:00:06 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Lotti from "./components/Lotti";
-import PdfPage from "./components/PdfPage";
-import Stopwatch from "./components/Refs";
-import DataTables from "./components/DataTables";
 import routes from "./routes";
-import { lib } from "crypto-js";
+
 // import LocalStore from "./components/LocalStore";
 
 function App() {
-  console.log(routes);
   return (
     <div className="App">
-      {/* <DataTables /> */}
-      {/* <LocalStore /> */}
-      {/* <Stopwatch /> */}
-      {/* <PdfPage /> */}
-      {/* <Lotti /> */}
-      {/* {routes.map((i) => {
-        console.log(i.url);
-      })} */}
-
       <BrowserRouter>
-        <ul className="ul-style">
+        <div className="main-style">
           {routes.map((route) => (
-            <li className="li-style">
-              <Link to={route.url}>{route.name}</Link>
-            </li>
+            <div className="dropdown">
+              <Link className="dropbtn" to={route.url} key={route.name}>
+                {route.name}
+              </Link>
+              <div className="dropdown-content">
+                {route.child
+                  ? route.child.map((route) => (
+                      <Link className="dropbtn" to={route.url} key={route.name}>
+                        {route.name}
+                      </Link>
+                    ))
+                  : ""}
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
+
         <Routes>
           {routes.map((route) => (
             <Route
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,30 @@ import DataTables from "./components/DataTables";
 import Lotti from "./components/Lotti";
 import PdfPage from "./components/PdfPage";
 import Stopwatch from "./components/Refs";
+import TsParticles from "./components/TsParticles";
 
 const routes = [
+  {
+    name: "Home",
+    components: <TsParticles />,
+    url: `/home`,
+  },
   {
     name: "Table",
     components: <DataTables />,
     url: `/data-tables`,
+    // child: [
+    //   {
+    //     name: "Sub-Table",
+    //     components: <DataTables />,
+    //     url: `/x1`,
+    //   },
+    //   {
+    //     name: "Sub-Child",
+    //     components: <Lotti />,
+    //     url: `/x2`,
+    //   },
+    // ],
   },
   {
     name: "LT",
